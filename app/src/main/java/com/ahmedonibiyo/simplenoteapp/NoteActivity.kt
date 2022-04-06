aa04055c2a4cb84bc8ae5fa3c04d4361d1dc79f2
Ahmed Onibiyo <Ahmed.onibiyo@babbangona.com>
2022-04-06 16:10:23 +0100

    Revert "Updated the app UI"
    This reverts commit e4c4662273a9d81ae190aa3e40e8ea8cdf4e1b0f.

--- app/src/main/java/com/ahmedonibiyo/simplenoteapp/NoteActivity.kt
+++ app/src/main/java/com/ahmedonibiyo/simplenoteapp/NoteActivity.kt
@@ -1,8 +1,6 @@
 package com.ahmedonibiyo.simplenoteapp
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.ahmedonibiyo.simplenoteapp.databinding.ActivityNoteBinding
 
@@ -15,16 +13,6 @@ class NoteActivity : AppCompatActivity() {
         binding = ActivityNoteBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.backIcon.setOnClickListener{
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
 
-        }
-
-        binding.editNote.setOnClickListener{
-            Toast.makeText(this,
-                "Keep calm we shall implement that feature soon...",
-                Toast.LENGTH_SHORT).show()
-        }
     }
 }
