e4c4662273a9d81ae190aa3e40e8ea8cdf4e1b0f
Ahmed <Ahmedabiodun4@gmail.com>
2022-04-06 16:02:41 +0100

    Updated the app UI

--- app/src/main/java/com/ahmedonibiyo/simplenoteapp/NoteActivity.kt
+++ app/src/main/java/com/ahmedonibiyo/simplenoteapp/NoteActivity.kt
@@ -1,6 +1,8 @@
 package com.ahmedonibiyo.simplenoteapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.ahmedonibiyo.simplenoteapp.databinding.ActivityNoteBinding
 
@@ -13,6 +15,16 @@ class NoteActivity : AppCompatActivity() {
         binding = ActivityNoteBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.backIcon.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
 
+        }
+
+        binding.editNote.setOnClickListener{
+            Toast.makeText(this,
+                "Keep calm we shall implement that feature soon...",
+                Toast.LENGTH_SHORT).show()
+        }
     }
 }
